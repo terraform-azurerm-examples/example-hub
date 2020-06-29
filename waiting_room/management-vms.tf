23693cb42a46c41b048fe6f8eb7a7504952043e3
Richard Cheney <richeney@microsoft.com>
2020-06-30 00:05:37 +0100

    Update management VMs to match updated modules

--- waiting_room/management-vms.tf
+++ waiting_room/management-vms.tf
@@ -1,61 +1,101 @@
-locals {
-  set_defaults = {
-    resource_group_name = azurerm_resource_group.hub.name
-    location            = azurerm_resource_group.hub.location
-    tags                = azurerm_resource_group.hub.tags
-    availability_set    = true
-    load_balancer       = true
-    subnet_id           = azurerm_subnet.SharedServices.id
-  }
+resource "azurerm_resource_group" "hub_management_vms" {
+  name     = "${var.hub}-management-vms"
+  location = var.location
+  tags     = var.tags
+}
 
+locals {
   vm_defaults = {
-    resource_group_name  = azurerm_resource_group.hub.name
-    location             = azurerm_resource_group.hub.location
-    tags                 = azurerm_resource_group.hub.tags
+    resource_group_name  = azurerm_resource_group.hub_management_vms.name
+    location             = azurerm_resource_group.hub_management_vms.location
+    tags                 = azurerm_resource_group.hub_management_vms.tags
     admin_username       = "ubuntu"
     admin_ssh_public_key = azurerm_key_vault_secret.ssh_pub_key["ubuntu"].value
     additional_ssh_keys  = []
     vm_size              = "Standard_B1ls"
     storage_account_type = "Standard_LRS"
+    identity_id          = azurerm_user_assigned_identity.hub.id
     subnet_id            = azurerm_subnet.SharedServices.id
     boot_diagnostics_uri = azurerm_storage_account.boot_diagnostics.primary_blob_endpoint
   }
 }
 
+//======================================================
+
 module "jumpbox" {
-  source   = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm?ref=v0.1"
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm" #?ref=v0.2"
+  // source   = "../../../modules/vm"
   defaults = local.vm_defaults
 
   name            = "jumpbox"
   source_image_id = data.azurerm_image.ubuntu_18_04.id
+  vm_size         = "Standard_B1ls"
 }
 
+//======================================================
 
-module "testbed_set" {
-  source   = "github.com/terraform-azurerm-modules/terraform-azurerm-set?ref=v0.1"
-  defaults = local.set_defaults
-
-  name = "testbed"
-}
-
-module "testbed" {
-  source   = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm?ref=v0.1"
+module "dc" {
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm" #?ref=v0.2"
+  // source   = "../../../modules/vm"
   defaults = local.vm_defaults
 
-  module_depends_on = [module.testbed_set]
-  attachType        = "All"
-  attach            = module.testbed_set.set_ids
-
-  names           = ["testbed-a", "testbed-b", "testbed-c"]
-  source_image_id = data.azurerm_image.ubuntu_18_04.id
+  availability_set_name = "dc"
+  names                 = ["dc-01", "dc-02"]
+  source_image_id       = data.azurerm_shared_image.ubuntu_18_04.id
+  subnet_id             = azurerm_subnet.DomainControllers.id
 }
 
-module "minimalargs" {
-  source              = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm?ref=v0.1"
-  resource_group_name = azurerm_resource_group.hub.name
+//======================================================
 
-  name                 = "minimalargs"
-  source_image_id      = data.azurerm_shared_image.ubuntu_18_04.id
-  subnet_id            = azurerm_subnet.SharedServices.id
-  boot_diagnostics_uri = azurerm_storage_account.boot_diagnostics.primary_blob_endpoint
+module "cfgmgmt_lb" {
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-load-balancer" #?ref=v0.2"
+  // source   = "../../../modules/lb"
+  defaults = local.vm_defaults
+
+  name = "cfgmgmt"
+
+  load_balancer_rules = [{
+    protocol      = "Tcp",
+    frontend_port = 22,
+    backend_port  = 22
+    },
+    {
+      protocol      = "Tcp",
+      frontend_port = 8443,
+      backend_port  = 443
+    }
+  ]
 }
+
+module "cfgmgmt" {
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vm" #?ref=v0.2"
+  // source   = "../../../modules/vm"
+  defaults = local.vm_defaults
+
+  availability_set_name                  = "cfgmgmt"
+  names                                  = ["cfgmgmt-01", "cfgmgmt-02"]
+  source_image_id                        = data.azurerm_image.ubuntu_18_04.id
+  load_balancer_backend_address_pool_ids = { "cfgmgmt" = module.cfgmgmt_lb.load_balancer_backend_address_pool_id }
+}
+
+//======================================================
+
+module "testbed_lb" {
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-load-balancer" #?ref=v0.2"
+  // source   = "../../../modules/lb"
+  defaults = local.vm_defaults
+  name     = "testbed"
+}
+
+module "testbed_vmss" {
+  source = "github.com/terraform-azurerm-modules/terraform-azurerm-linux-vmss" #?ref=v0.2"
+  // source   = "../../../modules/vmss"
+  defaults = local.vm_defaults
+
+  load_balancer_backend_address_pool_ids = [module.testbed_lb.load_balancer_backend_address_pool_id]
+  name                                   = "testbed"
+  instances                              = 3
+  source_image_id                        = data.azurerm_image.ubuntu_18_04.id
+}
+
+//======================================================
